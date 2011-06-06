9de533e9c10fcdfd70207ba31a8b5bbdb865a414
Vincent Bostoen <vincent.bostoen@gmail.com>
2011-06-06 22:34:25 +0200

    BaasPojo now implements Serializable in order to respect GWT Serialization policy

--- src/main/java/com/baas/shared/BaasPojo.java
+++ src/main/java/com/baas/shared/BaasPojo.java
@@ -1,10 +1,12 @@
 package com.baas.shared;
 
+import java.io.Serializable;
+
 /**
  *
  * @param <K> Key of the object
  */
-public interface BaasPojo<K> {
+public interface BaasPojo<K> extends Serializable{
 	public K getKey();
 	public void setKey(K key);
 }
